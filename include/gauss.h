a156b747a6ef88a879cb856e051236b395d579d4
nick0kcin <nikkholod@mail.ru>
2014-02-27 08:57:19 -0800

    Всё через матрицу

--- include/gauss.h
+++ include/gauss.h
@@ -1,54 +1,57 @@
 #include "vector"
 #include "iostream"
+#include "GeMatrix.h"
 using namespace std;
+//#define NO_SOLUTION -1
 #define MIN_ROOT -100
 #define MAX_ROOT 100
-bool Gauss(vector<vector<double> > v,int n,int m,vector<double>& res)
+/*vector<double>*/bool Gauss(GeMatrix v,int n,int m,vector<double>& res)
 {
-    vector<pair<double,double> > mains;
-    res.assign(n,0);
-    int t=0,j=0;
-    for(j=0;j<m&&t<n;t++)
-    {
-        double main_el=0;
-        int cur=-1;
-        for(int i=j;i<m;i++)
-            if(abs(v[i][t])>main_el)
-        {
-            main_el=abs(v[i][t]);
-            cur=i;
-        }
-            if(cur!=-1)
-            swap(v[cur],v[t]);
-            else
-            {
-                res[t]=MIN_ROOT+rand()%(MAX_ROOT-MIN_ROOT);
-                continue;
-            }
-            mains.push_back(make_pair(j,t));
-            for(int i=j+1;i<m;i++)
-                for(int k=n;k>=j;k--)
-                {
-                    v[i][k]-=v[j][k]*(v[i][t]/v[j][t]);
-                }
-            j++;
-    }
-    for(int i=m;i<n;i++)
-        res[i]=MIN_ROOT+rand()%(MAX_ROOT-MIN_ROOT);
-    for(int i=mains.size()-1;i>=0;i--)
-    {
-        double r=v[mains[i].first][n];
-        for(int j=mains[i].second+1;j<n;j++)
-            r-=v[mains[i].first][j]*res[j];
-        res[mains[i].second]=r/v[mains[i].first][mains[i].second];
-    }
-    for(int i=mains[mains.size()-1].first+1;i<m;i++)
-    {
-        double r=0;
-        for(int j=0;j<n;j++)
-            r+=v[i][j]*res[j];
-        if(r!=v[i][n])
-            return 0;
-    }
-return 1;
+	//vector<double> res;
+	vector<pair<double,double> > mains;
+	res.assign(n,0);
+	int t=0,j=0;
+	for(j=0;j<m&&t<n;t++)
+	{
+		double main_el=0;
+		int cur=-1;
+		for(int i=j;i<m;i++)
+			if(abs(v.At(i,t))>main_el)
+			{
+				main_el=abs(v.At(i,t));
+				cur=i;
+			}
+			if(cur!=-1)
+				v.Swap(cur,t);
+			else
+			{
+				res[t]=MIN_ROOT+rand()%(MAX_ROOT-MIN_ROOT);
+				continue;
+			}
+			mains.push_back(make_pair(j,t));
+			for(int i=j+1;i<m;i++)
+				for(int k=n;k>=j;k--)
+				{
+					v.Set(i,k,v.At(j,k)*(v.At(i,t)/v.At(j,t)));
+				}
+				j++;
+	}
+	for(int i=m;i<n;i++)
+		res[i]=MIN_ROOT+rand()%(MAX_ROOT-MIN_ROOT);
+	for(int i=mains.size()-1;i>=0;i--)
+	{
+		double r=v[mains[i].first][n];
+		for(int j=mains[i].second+1;j<n;j++)
+			r-=v.At(mains[i].first,j)*res[j];
+		res[mains[i].second]=r/v.At(mains[i].first,mains[i].second);
+	}
+	for(int i=mains[mains.size()-1].first+1;i<m;i++)
+	{
+		double r=0;
+		for(int j=0;j<n;j++)
+			r+=v.At(i,j)*res[j];
+		if(r!=v.At(i,n))
+			return 0;
+	}
+	return 1;
 }
