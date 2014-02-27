8871095b3ab6723aada4483858bb2ec4d92e86c6
nick0kcin <nikkholod@mail.ru>
2014-02-27 08:39:51 -0800

    Матрица

--- include/GeMatrix.h
+++ include/GeMatrix.h
@@ -3,6 +3,20 @@ class GeMatrix
 {
 public:
 	GeMatrix(void);
+	GeMatrix(int a,int b);
+	GeMatrix(const GeMatrix &a);
 	~GeMatrix(void);
+	int n,m;
+	double** Data;
+	void Set(int a,int b,double value);
+	void Clear();
+	bool operator==(GeMatrix& a);
+	void operator =(GeMatrix& a);
+	void operator<<(GeMatrix& a);
+	GeMatrix operator+(GeMatrix& a);
+	GeMatrix operator*(GeMatrix& a);
+	void operator+=(GeMatrix& a);
+	void Swap(int s1,int s2);
+	double At( int a,int b);
 };
 
